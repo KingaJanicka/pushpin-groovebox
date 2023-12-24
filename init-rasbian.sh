193347f2129a7b632e76d055f6d2bd3b0799b821
Kinga Janicka <kingajanicka7@gmail.com>
2023-12-24 22:28:25 +0000

    updating deps

---
+++ init-rasbian.sh
@@ -0,0 +1,10 @@
+sudo apt install cmake
+sudo apt install clang
+sudo apt install libx11-dev
+sudo apt install libxrandr-dev
+sudo apt install libxrandr-dev
+sudo apt install libxcursor-dev
+sudo apt install libfreetype-dev
+sudo apt-get install libasound2-dev
+sudo apt-get install libjack-dev
+
