d469d104d5904b4e5e6f0edd35674c7c9b168568
Ændra Rininsland <aendra.rininsland@ft.com>
2025-09-09 11:11:41 +0100

    Update create-image-with-backports.sh
    Trying to fix size-related error in generating release

--- scripts/image/create-image-with-backports.sh
+++ scripts/image/create-image-with-backports.sh
@@ -73,4 +73,5 @@ EOF
     ) | bash -c "cat >|$assets/my.plugins"
 echo $assets/my.plugins
 
-$sudo sdm --batch --customize --plugin @$assets/my.plugins --bootscripts --restart --regen-ssh-host-keys $IMAGE
+$sudo sdm --batch --customize --plugin @$assets/my.plugins --extend --xmb 4096 --bootscripts --restart --regen-ssh-host-keys $IMAGE
+$sudo sdm --shrink $IMAGE
