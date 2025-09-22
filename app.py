7a43bd0ecb683288f9d9359b0bedffb4d62bfe3e
KingaJanicka <kingajanicka7@gmail.com>
2025-09-22 15:51:15 +0100

    midi would crash with none input saved

--- app.py
+++ app.py
@@ -138,14 +138,17 @@ class PyshaApp(object):
             device_name=settings.get("default_midi_out_device_name", None)
         )
         iso_midi_in_device_name = None
-        for port in iso.get_midi_input_names():
-            if settings.get("default_midi_in_device_name") in port:
-                iso_midi_in_device_name = port
-                self.iso_midi_in = iso.MidiInputDevice(device_name=iso_midi_in_device_name)
-                self.global_timeline = iso.Timeline(output_device=iso.DummyOutputDevice(), clock_source=self.iso_midi_in)
-                # self.global_timeline.background()
-        if iso_midi_in_device_name == None:
-            self.global_timeline = iso.Timeline(self.tempo, output_device=iso.DummyOutputDevice())
+        # TODO: this is a bodge and should be made to work without the try/except
+        try:
+            for port in iso.get_midi_input_names():
+                if settings.get("default_midi_in_device_name") in port:
+                    iso_midi_in_device_name = port
+                    self.iso_midi_in = iso.MidiInputDevice(device_name=iso_midi_in_device_name)
+                    self.global_timeline = iso.Timeline(output_device=iso.DummyOutputDevice(), clock_source=self.iso_midi_in)
+                    # self.global_timeline.background()
+        except:
+            if iso_midi_in_device_name == None:
+                self.global_timeline = iso.Timeline(self.tempo, output_device=iso.DummyOutputDevice())
     
     
         self.init_notes_midi_in(
